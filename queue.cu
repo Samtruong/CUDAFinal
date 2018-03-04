d17256651e1461bad94df41f9769bcc9e275ba39
Vidush Vishwanath <vidushv@gmail.com>
2018-03-04 12:03:08 -0800

    queue cuda added

---
+++ queue.cu
@@ -0,0 +1,72 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+struct node
+{
+	int info;
+	struct node * next;
+}*front, *rear;
+
+class queue
+{
+public:
+	void push(int);
+	void print();
+	struct node* pop();
+
+	queue()
+	{
+		front = NULL;
+		rear = NULL;
+	}
+};
+
+void queue::push(int info)
+{
+	node *toInsert;
+	toInsert = new struct node;
+	toInsert -> info = info;
+	toInsert -> next = NULL;
+	if (front == NULL)
+	{
+		front = toInsert;
+	}
+	else
+	{
+		rear -> next = toInsert;
+	}
+	rear = toInsert;
+}
+
+
+struct node * queue::pop()
+{
+	node *toPop;
+	toPop = front;
+	front = front -> next;
+	return toPop;
+}
+
+void queue::print()
+{
+	node * ptr;
+	ptr = front;
+	while (ptr != NULL)
+	{
+		cout << ptr -> info << " ";
+		ptr = ptr -> next;
+	}
+	cout << endl;
+}
+
+__global__ void queueSim(queue q)
+{
+
+}
+
+int main ()
+{
+	queue q;
+	queueSim <<<1,1>>> (q);
+}
