e942ea552db8660f337cb93102400231b0448c0c
Samtruong <mstruong@ucdavis.edu>
2018-03-11 15:58:13 -0700

    branch and bound working

--- parallelSimplex.cu
+++ parallelSimplex.cu
@@ -1,44 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <curand.h>
 #include <curand_kernel.h>
+#include <iostream>
 
 #define TOTAL_BLOCKS 256;
 
+using namespace std;
 
-__device__ int policy(double *currentJob, double * leftChild, double * rightChild)
+__device__ int policy(double currentJob, double * leftChild, double * rightChild)
 {
   unsigned long long seed = threadIdx.x;
   unsigned long long sequence = threadIdx.x;
   unsigned long long offset = 0;
   curandState_t state;
   curand_init(seed,sequence,offset,&state);
-  *leftChild = *currentJob + 1;
-  *rightChild = *currentJob + 2;
+  if (currentJob == 1)
+  {
+    *leftChild = 2.;
+    *rightChild = 3.;
+  }
+  else if (currentJob == 2)
+  {
+    *leftChild = 4.;
+    *rightChild = 5.;
+  }
+  else if (currentJob == 3)
+  {
+    *leftChild = 6.;
+    *rightChild = 7.;
+  }
   int randomInt = curand(&state) % 10;
   // if ((randomInt) <= 9)
   //   return 0;
   // else
-     return 1;
+  if (currentJob == 1 || currentJob == 2 || currentJob == 3)
+    return 1;
+  else
+    return 0;
 }
 
 __global__ void branchAndBound(int upperBound, int* done1, int* done2,
-int* length1, int* length2, double** TableauList1, double** TableauList2)
+int* length1, int* length2, double* TableauList1, double* TableauList2)
 {
+  // printf("hello1\n");
   //__shared__ double* SimplexTableau;
   __shared__ int* length; //why pointer?
   __shared__ int* otherLength;
   __shared__ int* done;
-  __shared__ double** tablelist;
-  __shared__ double** otherTablelist;
-  __shared__ double* currentJob;
+  __shared__ double* tablelist;
+  __shared__ double* otherTablelist;
+  __shared__ double currentJob;
   __shared__ int action;
   __shared__ bool localDone;
+  // printf("hello2\n");
 //Assign shared memory to passed in values.
 
-
-  //while (*done2 == totalBlocks) //&& (length1 == 0))
-   while(!(((*done2 == 256) && (length1 == 0)) || ((*done1 == 256) && (length2 == 0))))//when there are still jobs
+int numblocks = 2;
+  // printf("done2 %d, length1 %d, done1 %d, length2 %d\n", *done2, *length1, *done1, *length2);
+   while(!(((*done2 == numblocks) && (*length1 == 0)) || ((*done1 == numblocks) && (*length2 == 0))))//when there are still jobs
   {
-    if (*done1 == 256)
+     // printf("About to enter while loop\n");
+     localDone = 0;
+    if (*done1 == numblocks) //start with 2
     {
       done = done2;
       tablelist = TableauList2;
@@ -48,7 +72,7 @@ int* length1, int* length2, double** TableauList1, double** TableauList2)
       *done1 = 0;
     }
 
-    else
+    else //start with 1
     {
       done = done1;
       tablelist = TableauList1;
@@ -57,11 +81,11 @@ int* length1, int* length2, double** TableauList1, double** TableauList2)
       otherLength = length2;
       *done2 = 0;
     }
-
-    while (*done != 256)
+    while (*done != numblocks)
     {
       if(!localDone)
       {
+        // printf("inside if\n");
         int index = blockIdx.x;
         int stride = gridDim.x;
         for (int i = index; i < *length; i+= stride)
@@ -72,18 +96,24 @@ int* length1, int* length2, double** TableauList1, double** TableauList2)
           }
           __syncthreads();
           double * leftChild, * rightChild;
+          leftChild = (double*) malloc(sizeof(double));
+          rightChild = (double*) malloc(sizeof (double));
           action = policy(currentJob, leftChild, rightChild);
-
-
+          // printf("otherLength %d\n", *otherLength);
+          // printf("current job %f\n leftChild %f\n rightChild %f\n", currentJob, *leftChild, *rightChild);
+          // printf("otherLength %d\n", *otherLength);
           if (threadIdx.x == 0)
           {
-            if(action == 0) // STOP
+            if(action == 0) // BOUND
               continue;
             else // BRANCH
             {
               atomicAdd(otherLength, 2);
-              *otherTablelist[*otherLength - 2] = *leftChild;
-              *otherTablelist[*otherLength - 1] = *rightChild;
+              // printf("otherLength %d\n", *otherLength);
+              otherTablelist[(*otherLength) - 2] = *leftChild;
+              otherTablelist[(*otherLength) - 1] = *rightChild;
+              // printf("leftChild %f\n", *leftChild);
+              // printf("rightChild %f\n", *rightChild);
             }
           }
         }
@@ -102,12 +132,40 @@ int* length1, int* length2, double** TableauList1, double** TableauList2)
 
 int main ()
 {
-  double **TableauList1, ** TableauList2;
-  int * done1 = new int(0);
-  int* done2 = new int(1);
-  int * length1 = new int(1);
-  int * length2 = new int(0);
-  branchAndBound<<<256, 1>>> (10, done1, done2, length1, length2, TableauList1, TableauList2);
+  double *TableauList1, * TableauList2;
+  cudaMallocManaged(&TableauList1, sizeof(double) * 10);
+  cudaMallocManaged(&TableauList2, sizeof(double) * 10);
+  *TableauList1 = 1.0;
+  int * done1;
+  cudaMallocManaged(&done1, sizeof(int));
+  *done1 = 0;
+  int* done2;
+  cudaMallocManaged(&done2, sizeof(int));
+  *done2 = 1;
+  int * length1;
+  cudaMallocManaged(&length1, sizeof(int));
+  *length1 = 1;
+  int * length2;
+  cudaMallocManaged(&length2, sizeof(int));
+  *length2 = 0;
+  branchAndBound<<<2, 1>>> (10, done1, done2, length1, length2, TableauList1, TableauList2);
 
+  cudaDeviceSynchronize();
+  for (int i = 0; i < 10; i++)
+  {
+    cout << TableauList1[i] << " ";
+  }
+    cout << endl;
+
+  for (int i = 0; i < 10; i++)
+    cout << TableauList2[i] << " ";
+    cout << endl;
+
+  cudaFree(done1);
+  cudaFree(done2);
+  cudaFree(length1);
+  cudaFree(length2);
+  cudaFree(TableauList1);
+  cudaFree(TableauList2);
   return 0;
 }
